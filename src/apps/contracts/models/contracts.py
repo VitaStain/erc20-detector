93eeffb818e16493c910eefdbf5c50975c6808ab
vitaly <vitalikstain@inbox.ru>
2024-07-09 16:44:32 +0300

    feat: standard contract relationship

--- src/apps/contracts/models/contracts.py
+++ src/apps/contracts/models/contracts.py
@@ -1,6 +1,6 @@
-from typing import List, TYPE_CHECKING
+from typing import TYPE_CHECKING
 
-from sqlalchemy import String, Text, Boolean
+from sqlalchemy import String, Text, Boolean, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.apps.urils.constants import ContractStatus
@@ -13,14 +13,19 @@ if TYPE_CHECKING:
 class Contract(Base):
     __tablename__ = "contracts"
 
-    contract_address: Mapped[str] = mapped_column(String(50))
+    contract_address: Mapped[str] = mapped_column(String(50), unique=True)
     source_code: Mapped[str] = mapped_column(Text)
     is_erc20: Mapped[bool] = mapped_column(Boolean, default=False)
     erc20_version: Mapped[str] = mapped_column(String(50), default=None, nullable=True)
     status: Mapped[ContractStatus] = mapped_column(
         String(20), default=ContractStatus.WAIT_PROCESSING
     )
-    standard: Mapped[List["Standard"]] = relationship(
-        secondary="contract__standard",
+    standard_id: Mapped[int] = mapped_column(
+        ForeignKey(
+            "standards.id",
+            ondelete="CASCADE",
+        )
+    )
+    standard: Mapped["Standard"] = relationship(
         back_populates="contracts",
     )
